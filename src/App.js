af10fb7ddf9919aa8bc1db1307a65f1519842373
gitSambhal <sambhal@outlook.com>
2024-03-05 01:48:16 +0530

    feature: added google analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import ReactGA from "react-ga4";
 import './style.css';
 import VCFSplitter from './vcf-splitter';
+
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
 
 export default function App() {
   return <VCFSplitter></VCFSplitter>;
